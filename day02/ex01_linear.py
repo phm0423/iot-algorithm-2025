81f3dd7daa2aef8f81a17f9a9a16dd76ac3ebeb2
phm0423 <qkrgusalsok@gmail.com>
2025-02-14 17:19:56 +0900

    자료구조 2일차

---
+++ day02/ex01_linear.py
@@ -0,0 +1,42 @@
+# p115 1번 예제
+
+# 전역 변수
+katok = [('다현', 200), ('정연', 150), ('쯔위', 90), ('사나', 30), ('지효', 15)]
+
+## 함수
+def find_insert_data(ktFriend, ktCount):
+    findPos = -1
+    for i in range(len(katok)):
+        tuPle = katok[i]
+        if ktCount >= tuPle[1]:
+            findPos = i
+            break
+    if findPos == -1 : 
+        findPos = len(katok)
+
+    insert_data(findPos, (ktFriend, ktCount))
+
+
+def insert_data(position, friend) : 
+    if position < 0 or position > len(katok) :
+        print('데이터를 삽입할 범위를 벗어났습니다.')
+        return
+    
+    katok.append(None)
+    kLen = len(katok)
+
+    for i in range(kLen - 1, position, -1):
+        katok[i] = katok[i-1]
+        katok[i-1] = None
+
+    katok[position] = friend
+
+
+## 메인코드
+if __name__ == '__main__':
+    
+    while True :
+        data = input('친구->')
+        count = int(input('횟수->'))
+        find_insert_data(data, count)
+        print(katok)
